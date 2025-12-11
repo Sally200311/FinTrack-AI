b0a087b19ea1fb061cd9cae89b58f09b2785a45a
Sally200311 <youz20031111@gmail.com>
2025-12-11 19:55:14 +0800

    feat: Hardcode Firebase config
    Replaces environment variables for Firebase configuration with hardcoded values in `services/firebase.ts`. This change also removes the associated secrets and deployment logic from the GitHub Actions workflow, simplifying the build and deployment process for local development or environments where these secrets are not needed.

--- services/firebase.ts
+++ services/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY,
-  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDDPD6qTHO9_3LCbBtrmZ2d3hx46pLqfzI",
+  authDomain: "fintrack-c130a.firebaseapp.com",
+  projectId: "fintrack-c130a",
+  storageBucket: "fintrack-c130a.firebasestorage.app",
+  messagingSenderId: "528377724610",
+  appId: "1:528377724610:web:19cf5815b14ebe5d7e855b",
   measurementId: "G-898WLQYGZS"
 };
 
